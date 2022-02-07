fd8fd448e1e45d085e6759724093d3f73f3eebb9
Carlos <carlos96831@gmail.com>
2022-02-07 11:15:04 -0800

    created mutations.js and queries.js files. Added Appollo to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {AppolloProvider} from '@apollo/react-hooks';
+import ApolloClient from 'appollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  request: operation =>{
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers:{
+        authorization: token ? `Bearer ${token}`  : ''
+      }
+    });
+  },
+  uri:'/graphql'
+});
+
 function App() {
   return (
+    <AppolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +32,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </AppolloProvider>
   );
 }
 
